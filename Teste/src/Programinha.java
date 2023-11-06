bdca34bf4a8d166fbd267f12196ee992db648280
Gabriel Pagote <gpoliveira238@outlook.com>
2023-11-06 14:55:19 -0300

    initial commit

---
+++ Teste/src/Programinha.java
@@ -0,0 +1,11 @@
+import javax.swing.JOptionPane;
+
+public interface Programinha {
+
+	public static void main(String[] args) {
+		JFrame jframe = new Jframe();
+		JOptionPane.showMessageDialog(null, "Qual é o amor da sua vida?");
+		
+	}
+	
+}
---
+++ clinica/src/sistema/TelaPrincipal.java
@@ -0,0 +1,11 @@
+package sistema;
+
+public class TelaPrincipal {
+
+	//Tudo começa a partir do Main (sempre em letra minuscula)
+	
+	public static void main(String[] args) {
+		
+	}
+	
+}
