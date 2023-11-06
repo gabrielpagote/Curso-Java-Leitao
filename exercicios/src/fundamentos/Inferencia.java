032eae497338c3503c8d6bc10a74bfa043868921
Gabriel Pagote <60767798+gabrielpagote@users.noreply.github.com>
2023-11-06 15:15:31 -0300

    Removendo testes e adicionando inferencia

---
+++ exercicios/src/fundamentos/Inferencia.java
@@ -0,0 +1,26 @@
+package fundamentos;
+
+public class Inferencia {
+	
+	public static void main(String[] args) {
+		
+		double a = 4.5;
+		System.out.println(a);
+		
+		//a linguagem irá determinar o tipo da variável com base no que escrevemos "var"
+		//quando utilizamos var precisamos inicializar a variável logo em seguida, na mesma linha
+		var b = 12.57;
+		System.out.println(b);
+		
+		var c = "Texto";
+		System.out.println(c);
+		
+		c = "Outro texto";
+		System.out.println(c);
+		
+		double d; //declarando variavel
+		d = 123.65; //inicializando variavvel
+		
+		
+	}
+}
